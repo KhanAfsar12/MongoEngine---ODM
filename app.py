57b8f7456fb3a691e8b45783a84ebd6bc626b760
KhanAfsar12 <ka484564@gmail.com>
2025-07-04 12:28:22 +0530

    Update work has done

--- app.py
+++ app.py
@@ -7,10 +7,24 @@ connect(
     host = "localhost",
     port = 27017,
 )
-try:
-    user = User(email = "afsar@easemyai.com")
-    user.first_name = "Afsar"
-    user.last_name = "Khan"
-    user.save()
-except NotUniqueError as e:
-    print("Email already exists")
+
+# Add 
+# try:
+#     user = User(email = "afsar@easemyai.com")
+#     user.first_name = "Afsar"
+#     user.last_name = "Khan"
+#     user.save()
+# except NotUniqueError as e:
+#     print("Email already exists")
+
+
+# Update
+user = User.objects(email = "afsar@easemyai.com")
+user.update(first_name="Ishtiyaque", last_name="Hussain")
+
+user2 = User.objects(email = "afsar@easemyai.com")
+fields = {
+    "first_name": "Afsar",
+    "last_name": "Khan"
+}
+user2.update(**fields)
