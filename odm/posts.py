c8739ccc282a9ccfcb1bad4043c590f4e200b30f
KhanAfsar12 <ka484564@gmail.com>
2025-07-04 16:07:15 +0530

    Configuration in Posts

--- odm/posts.py
+++ odm/posts.py
@@ -1,17 +1,35 @@
-from mongoengine import Document, StringField, DictField, ListField, DateTimeField
+from mongoengine import Document, StringField, DictField, ListField, DateTimeField, EmbeddedDocument, EmbeddedDocumentField, ObjectIdField
+
+POST_STATUS = ('pending', 'published', 'deleted', 'draft',)
+
+
+class PostMetatag(EmbeddedDocument):
+    title = StringField()
+    desccription = StringField()
+    encode = StringField(default='utf-8')
+
+    def __str__(self):
+        return f"{self.title}"
+    
+
+class PostCategory(EmbeddedDocument):
+    id = ObjectIdField()
+    title = StringField()
+
 
 class Posts(Document):
     title = StringField()
     url = StringField()
     content = StringField()
-    metatag = DictField()
-    categorys = ListField()
-    authors = ListField()
-    status = StringField()
+    metatag = DictField(EmbeddedDocumentField(PostMetatag))
+    categorys = ListField(EmbeddedDocumentField(PostCategory))
+    authors = ListField(ObjectIdField())
+    status = StringField(choices=POST_STATUS)
     updated_at = DateTimeField()
     created_at = DateTimeField()
 
     meta = {
+        'collection': 'custom-post-collection',
         'auto_create_index': True,
         'index_background': True,
         'indexes': [{
